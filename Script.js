f397e45051a80df8e7ac3b6bce71c93b09ce84bc
Chacon042024 <167449193+Chacon042024@users.noreply.github.com>
2024-04-18 17:29:19 +0200

    Create Script.js
    Script.js

---
+++ Script.js
@@ -0,0 +1,11 @@
+// Se puede añadir código adicional aquí para integrar funcionalidades específicas del chatbot
+
+// Función de inicialización del chatbot (simulación)
+function initializeChatbot() {
+    console.log("Chatbot initialized and ready!");
+}
+
+// Llamar a la función de inicialización cuando la página cargue completamente
+window.onload = function() {
+    initializeChatbot();
+};
